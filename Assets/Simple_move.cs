becbbf8b286d42906234841d32c7c8f91f71068f
5SPectra$ <Ninja.videos2@gmail.com>
2017-11-27 14:32:27 -0200

    Colocando coisa dentro

---
+++ Assets/Simple_move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simple_move : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	void FixedUpdate () {
+
+		float x = Input.GetAxis ("Horizontal"); //* Time.fixedDeltaTime;
+			float y = Input.GetAxis ("Vertical");// * Time.fixedDeltaTime;
+
+		print (x);
+		rb.AddForce (Vector2.right, );
+	}
+}
