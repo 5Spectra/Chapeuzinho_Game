3a99829ef4a17f808fa89e13a277717613c5d5bf
5SPectra$ <Ninja.videos2@gmail.com>
2017-12-05 17:21:42 -0200

    new

---
+++ Assets/Scripts/Player_Collisions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Collisions : MonoBehaviour {
+
+	public
+
+	void OnTriggerEnter2D (Collider2D coll){
+		if (coll.gameObject.tag == "doce") {
+			//pontos += 1;
+			Destroy(coll.gameObject);
+		}
+	}
+}
