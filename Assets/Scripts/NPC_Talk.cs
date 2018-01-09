94a61eca6d22b92968a1172ca4240c56e2dd8765
5SPectra$ <Ninja.videos2@gmail.com>
2018-01-09 17:25:10 -0200

    re upload

---
+++ Assets/Scripts/NPC_Talk.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC_Talk : MonoBehaviour {
+
+	public GameObject e_btn, text_box; 
+	public Text texto;
+
+	bool perto, axisDown;
+	int num_fala;
+
+	[TextArea()]
+	public string[] falas; 
+
+	void Start () {
+		e_btn.SetActive(false);
+		text_box.SetActive (false);
+	}
+
+	void FixedUpdate(){
+
+		if (Input.GetAxisRaw ("Interagir") > 0 && perto)
+			if (axisDown == false) {
+			text_box.SetActive (true);
+			texto.text = falas [num_fala];
+			num_fala += 1;
+			axisDown = true;
+			} 
+		if (Input.GetAxisRaw ("Interagir") == 0) {
+			axisDown = false;
+		}
+
+		if (falas.Length - 1 < num_fala) {
+			num_fala = 0;
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D coll)
+	{
+		e_btn.SetActive(true);
+		perto = true;
+	}
+
+	void OnTriggerExit2D (Collider2D coll){
+		e_btn.SetActive(false);
+		text_box.SetActive (false);
+		perto = false;
+		num_fala = 0;
+	}
+}
