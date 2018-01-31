2686d6a13d7813099c92d307187f1698e84c4ae5
5SPectra$ <Ninja.videos2@gmail.com>
2018-01-31 17:31:37 -0200

    Update

--- Assets/Scripts/conversa_2.cs
+++ Assets/Scripts/conversa_2.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class conversa_2 : MonoBehaviour {
 
-	bool axisDown;
+	bool axisDown, first;
 
 	public Animator chapeu, lenhador, lobo; 
 
@@ -13,12 +13,13 @@ public class conversa_2 : MonoBehaviour {
 
 	public GameObject btn1, btn2; 
 
-	public Text text1, text2;
+	public Text text1, text2, falando1, falando2;
 	 
 	int conversa_level, num_conversa;
 
 	[TextArea(1,3)]
 	public string[] falasC1, falasC2;
+	public string[] conv1F, conv2F;
 
 	void Start () {
 
@@ -37,7 +38,7 @@ public class conversa_2 : MonoBehaviour {
 
 	void Update () {
 
-		print ("level " + conversa_level + " | num " + num_conversa + " | Falas " + falasC1.Length);
+		//print ("level " + conversa_level + " | num " + num_conversa + " | Falas " + falasC1.Length);
 
 		if (Input.GetAxisRaw ("Interagir") > 0)
 			if (axisDown == false){
@@ -50,13 +51,17 @@ public class conversa_2 : MonoBehaviour {
 
 		//conversa entre chapeu e lenhador
 		if (num_conversa == 1) {
-			if (conversa_level == 0) StartCoroutine (Wait_Chapeu ());
+			if (conversa_level == 0 && first == false) StartCoroutine (Wait_Chapeu ());
 
-			if (conversa_level < falasC1.Length) text1.text = falasC1[conversa_level];
+			if (conversa_level < falasC1.Length) {
+				text1.text = falasC1 [conversa_level];
+				falando1.text = conv1F [conversa_level];
+			}
 
-			if (conversa_level == falasC1.Length) {
-				num_conversa = 2;
+			if (conversa_level >= falasC1.Length) {
 				conversa_level = 0;
+				num_conversa = 2;
+				first = false;
 			}
 
 		}
@@ -64,42 +69,50 @@ public class conversa_2 : MonoBehaviour {
 		//conversa entre lenhador e lobo
 		if (num_conversa == 2) {
 			
-			if (conversa_level == 0) StartCoroutine (Wait_Lobo ());
+			if (conversa_level == 0 && first == false) StartCoroutine (Wait_Lobo ());
 
-			if (conversa_level < falasC2.Length) text2.text = falasC2 [conversa_level];
+			if (conversa_level < falasC2.Length) {
+				text2.text = falasC2 [conversa_level];		
+				falando2.text = conv2F [conversa_level];
+			}
 
-			if (conversa_level == falasC2.Length) {
-				num_conversa = 3;
+			if (conversa_level >= falasC2.Length) {
 				conversa_level = 0;
+				num_conversa = 3;
 			}
 		}
 
 		//fim da conversa
 		if (num_conversa == 3) {
-			Dialogos.SetActive (false);
+			//Dialogos.SetActive (false);
 			combat.SetActive (true);
+			Destroy (Dialogos, 1);
 			GetComponent<conversa_2> ().enabled = false;
 		}
 
 	}
 
 	IEnumerator Wait_Chapeu(){
+		first = true;
 
 		yield return new WaitForSeconds (2.5f);
 		dialogo1.SetActive (true);
 		chapeu.SetBool ("isParada", true);
-		//conversa_level = 0;
+
 	}
 
 	IEnumerator Wait_Lobo(){
+		first = true;
+
+		dialogo1.SetActive(false);
 		lobo.enabled = true;
 		lenhador.SetBool ("Olhar_lobo", true);
-		dialogo1.SetActive(false);
 
 		yield return new WaitForSeconds (2f);
 
+		lobo.SetBool ("isParado", true);
 		dialogo2.SetActive (true);
-		//conversa_level = 0;
+
 	}
 
 }
